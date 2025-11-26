3bf8c737d145c9b6714e400b1bd2b5a0438b64e9
abdelatif.qa <abdelatifmouri.tech@gmail.com>
2025-11-25 19:47:56 -0800

    run test on headless

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: true,
     trace: 'on-first-retry',
   },
 
@@ -38,7 +39,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        
+       },
     },
 
     // {
